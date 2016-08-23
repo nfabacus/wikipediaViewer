58278c8ea3e17c527d1a6512d94940c8d2697e96
noby <n.fujioka@abacusmaths.info>
2016-08-23 04:53:19 +0100

    controlled input

--- src/ContactsList.js
+++ src/ContactsList.js
@@ -2,14 +2,31 @@ import React from 'react';
 import Contact from './Contact';
 
 class ContactsList extends React.Component {
-  render() {
+  constructor(){
+    super();
+    this.state = {
+      search: 'My default contact'
+    };
+  }
 
+  updateSearch(event) {
+    this.setState({search: event.target.value.substr(0, 40)});
+  }
+
+  render() {
     return (
-      <ul>
-        {this.props.contacts.map((contact)=>{
-          return <Contact contact={contact} key={contact.id}/>
-        })}
-      </ul>
+      <div>
+        <ul>
+          {this.props.contacts.map((contact)=>{
+            return <Contact contact={contact} key={contact.id}/>
+          })}
+        </ul>
+        <input type="text"
+               value={this.state.search}
+               onChange={this.updateSearch.bind(this)}/>
+
+      </div>
+
     )
   }
 }
